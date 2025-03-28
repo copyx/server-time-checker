a8c68c5f22a68bac2d9ea134d3b8c4067b1e2549
Jingi Kim <copyx00@gmail.com>
2025-03-28 10:16:55 +0900

    feat: add latency

--- main.go
+++ main.go
@@ -20,19 +20,32 @@ func main() {
 	defer ticker.Stop()
 
 	for {
-		select {
-		case <-ticker.C:
-			resp, err := http.Head(url)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Local:", time.Now().Format(time.RFC1123), ", Server:", resp.Header["Date"])
+		<-ticker.C
+		start := time.Now().UnixMilli()
+
+		resp, err := http.Head(url)
+		if err != nil {
+			fmt.Println(err)
 		}
+
+		latency := time.Now().UnixMilli() - start
+		respDates := resp.Header["Date"]
+		if len(respDates) < 1 {
+			fmt.Println("There is no date header value in response")
+			os.Exit(1)
+		}
+		localTime := time.Now().Format(time.RFC1123Z)
+		serverTime, err := time.Parse(time.RFC1123, respDates[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf("Local: %s, Server: %s, Latency: %dms\n", localTime, serverTime.Format(time.RFC1123Z), latency)
 	}
 }
 
 func printUsages() {
-	var usages = [...]string{
+	usages := [...]string{
 		"server-time-checker is a CLI command to check the server time of target http server.",
 		"It will check the server time every seconds infinitely by default",
 		"",
